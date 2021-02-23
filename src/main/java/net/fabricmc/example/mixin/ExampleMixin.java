4ca6f366fd43d55206de940cb42ba10756cd3567
DbpGaming <LbpDbpGaming@gmail.com>
2021-02-23 15:27:49 -0600

    initial torch placement

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,15 +1,26 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.fabricmc.example.TorchPlacement;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.ActionResult;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+import java.util.Set;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PickaxeItem.class)
+public class ExampleMixin extends MiningToolItem {
+	protected ExampleMixin(float attackDamage, float attackSpeed, ToolMaterial material, Set<Block> effectiveBlocks, Settings settings) {
+		super(attackDamage, attackSpeed, material, effectiveBlocks, settings);
+	}
+
+	@Override
+	public ActionResult useOnBlock(ItemUsageContext context) {
+			TorchPlacement.placeTorch(context);
+		return ActionResult.PASS;
 	}
 }
